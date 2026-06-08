6e6427b510cefca12ef4dc453c6e9200aff02bfe
adhamsaber491-max <adhamsaber491@gmail.com>
2026-06-08 21:18:35 +0300

    Update day2.py
    تحديث معاد السفر شهر 9 سنة 2028

--- day2.py
+++ day2.py
@@ -1,6 +1,6 @@
 name = input("اكتب اسمك: ")
-age = int(input("عندك كام سنة: "))
-future_age = age + 2
+age = input("عندك كام سنة: ")
 print("أهلا يا " + name)
-print("كمان سنتين هيبقى عندك " + str(future_age) + " سنة")
-print("فاضلك " + str(22 - future_age) + " سنة وتوصل روسيا")
+print("كمان سنتين هيبقى عندك " + str(int(age) + 2) + " سنة")
+print("معاد سفرك لروسيا: شهر 9 سنة 2028")
+print("فاضلك سنتين بالظبط")
