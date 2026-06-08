c5fe02cb7460783159e0fae42485883b6f15ed34
adhamsaber491-max <adhamsaber491@gmail.com>
2026-06-08 21:09:51 +0300

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,6 @@
+name = input("اكتب اسمك: ")
+age = int(input("عندك كام سنة: "))
+future_age = age + 2
+print("أهلا يا " + name)
+print("كمان سنتين هيبقى عندك " + str(future_age) + " سنة")
+print("فاضلك " + str(22 - future_age) + " سنة وتوصل روسيا")
